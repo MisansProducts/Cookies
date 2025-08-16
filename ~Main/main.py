a674aecb6f6ddd3d299002e5ce59215dd999a8ef
Alex Akoopie <alexakoopie@gmail.com>
2025-08-15 21:36:44 -0700

    Updated Main
    Added car.py.

--- ~Main/main.py
+++ ~Main/main.py
@@ -4,23 +4,6 @@
 import random
 
 #======Functions======
-def myFunc(degree, t, value):
-	print(f"How far did a car travel if it was traveling at {value} m/s^{degree} for {t} seconds?")
-	#Suppose (1/a)bc^d
-	#Then b/a multiplies by c^d
-	d = {} # Dictionary of coeffients b/a
-	d[0] = value/degree # Initial coefficient b/a
-
-	#Then introduce c^d
-	p = d[0] * t ** degree # Distance Traveled
-
-	#Loop through the degree with natural number i
-	for i in range(1, degree):
-		d[i] = d[i - 1]/(i) # This key is defined by the previous key (b/a)/i
-		p = d[i] * t ** (degree) # New disntance traveled.
-	
-	print(f"The car traveled {p:.2f} meters.")
-
 #Multiplicative Persistence
 def multP(num, count = 0):
 	product = 1
