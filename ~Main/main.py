f2f953cb21f02a237a8810cab931e4e5942d545f
Alex Akoopie <alexakoopie@gmail.com>
2025-09-21 22:19:11 -0700

    Updated Main
    Added b_sort_new.py.

--- ~Main/main.py
+++ ~Main/main.py
@@ -4,20 +4,6 @@
 import random
 
 #======Functions======
-#Bubble Sort Function (New)
-def bSortNew(amount = 10, min = 0, max = 10):
-	l = list(random.randint(min, max) for n in range(amount))
-	print("List: ")
-	print(*l)
-	for i in range(len(l)):
-		for j in range(len(l) - 1):
-			if l[j] > l[j + 1]:
-				temp = l[j]
-				l[j] = l[j + 1]
-				l[j + 1] = temp
-	print("Sorted: ")
-	print(*l)
-
 #Selection Sort Function
 def s_sort():
 	l = [s for s in input("Type list elements: ").split()]
