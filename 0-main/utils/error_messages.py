9fc596c081826292e0d4b4d7abc48f282fbd63a8
Alex Akoopie <alexakoopie@gmail.com>
2026-01-26 21:29:32 -0800

    Updated Main
    Removed unused error messages and input validators.

--- 0-main/utils/error_messages.py
+++ 0-main/utils/error_messages.py
@@ -7,12 +7,8 @@ ERROR_MESSAGES = {
     'invalid_number': "Error: Please enter a valid number!",
     'min_value': "Error: {parameter_name} must be at least {min_val}!",
     'max_value': "Error: {parameter_name} must be at most {max_val}!",
-    'range_value': "Error: {parameter_name} must be between {min_val} and {max_val}!",
-    'positive': "Error: Value must be positive!",
-    'non_negative': "Error: Value cannot be negative!",
-    'format': "Error: Incorrect format."
+    'range_value': "Error: {parameter_name} must be between {min_val} and {max_val}!"
 }
-
 
 def get_error_message(key, **kwargs):
     """Get formatted error message by key."""
--- 0-main/utils/input_validators.py
+++ 0-main/utils/input_validators.py
@@ -40,26 +40,3 @@ def get_valid_float(prompt, min_val=None, parameter_name="Value"):
     except KeyboardInterrupt:
         print()
         sys.exit(1)
-
-def get_multiple_numbers(prompt_template, count, validator=None):
-    """Get multiple numbers with consistent validation."""
-    numbers = []
-    for i in range(1, count + 1):
-        if validator:
-            num = validator(prompt_template.format(i))
-        else:
-            num = get_valid_float(prompt_template.format(i))
-        numbers.append(num)
-    return numbers
-
-def get_list_input(prompt, separator=" ", validator=None):
-    """Get list input with optional validation."""
-    try:
-        items = input(prompt).split(separator)
-        if validator:
-            return [validator(item) for item in items]
-        return items
-    except ValueError:
-        print(get_error_message('format'))
-    except KeyboardInterrupt:
-        print()
